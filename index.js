b051d8b2dddd37a98fdb7b89922fc88d3e77e04a
wiresboy-exe <57566773+wiresboy-exe@users.noreply.github.com>
2021-12-17 14:11:19 +0000

    fix styles a bit

--- index.js
+++ index.js
@@ -1,16 +1,6 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res, next) => {
-    res.sendFile(__dirname + '/browser.html');
-})
-
-app.get('/api/v1/:ver/:header', (req, res, next) => {
-    res.sendFile(__dirname + '/data/' + req.params.ver + '/' + req.params.header + '.json')
-})
-
-app.get('/api/v1/:ver', (req, res, next) => {
-    res.sendFile(__dirname + '/names/' + req.params.ver + '.json')
-})
+app.use(express.static(__dirname));
 
 app.listen(80);
