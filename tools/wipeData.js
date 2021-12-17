bf08936e9a4bb7df24520ca2d47b8d7e2dab776f
wiresboy-exe <57566773+wiresboy-exe@users.noreply.github.com>
2021-12-17 09:16:52 +0000

    update

--- tools/wipeData.js
+++ tools/wipeData.js
@@ -3,7 +3,11 @@ const fs = require('fs');
 let dir = fs.readdirSync('data')
 
 dir.forEach(file => {
-    fs.unlinkSync('data/'+file);
+    let dir1 = fs.readdirSync('data/'+file)
+
+    dir1.forEach(file2 => {
+        fs.unlinkSync('data/'+file+'/'+file2);
+    })
 })
 
 // DON'T RUN THESE IN THIS FOLDER (put them into the folder with index.js then run node wipeData.js)
